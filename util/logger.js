c235d887872234fff66689d429bc787edcc16d95
Salman Hasni <salman@justplayss.com>
2025-03-27 12:30:09 -0400

    refreshed jp monitor

--- util/logger.js
+++ util/logger.js
@@ -1,45 +1,23 @@
-var log = require('npmlog');
-var mailer = require('./mailer');
-const Slack = require('./slack');
+import log from 'npmlog';
+import Slack from './slack.js';
 
 let slack;
 
-exports.notifyError = function (error, config, subject) {
-    // Fallback to generic subject
-    subject = subject || 'Health Check Failed';
+export function notifyError(error, config, subject) {
+  // Fallback to generic subject
+  subject = subject || 'Health Check Failed';
 
-    // Log error to CLI
-    log.error('mongomonitor', new Date(), error.message);
+  // Log error to CLI
+  log.error('mongomonitor', new Date(), error.message);
 
-    // Email the error if smtp settings present
-    if (config.smtp !== undefined) {
-        // Generate e-mail using mailgen
-        var mail = mailer.generate({
-            body: {
-                title: subject,
-                intro: error.message,
-                outro: 'We thank you for choosing mongomonitor.'
-            }
-        });
-
-        // Send mail via mailer
-        mailer.sendMail({
-            from: config.smtp.address,
-            to: config.smtp.address,
-            subject: `[mongomonitor] ${subject}`,
-            html: mail.html,
-            text: mail.text
-        }, config);
+  // Send Slack message if webhook settings present
+  if (config.slack !== undefined) {
+    if (slack === undefined) {
+      slack = new Slack(config.slack);
     }
-
-    // Send Slack message if webhook settings present
-    if (config.slack !== undefined) {
-        if (slack === undefined) {
-            slack = new Slack(config.slack);
-        }
-        slack.sendWebhookMessage(subject, error)
-            .catch(function (slackError) {
-                log.error('mongomonitor', new Date(), slackError.message)
-            });
-    }
-};
+    slack.sendWebhookMessage(subject, error)
+    .catch((slackError) => {
+      log.error('mongomonitor', new Date(), slackError.message);
+    });
+  }
+}
--- util/slack.js
+++ util/slack.js
@@ -1,53 +1,51 @@
-'use strict';
+import { IncomingWebhook } from '@slack/webhook';
 
-const { IncomingWebhook } = require('@slack/webhook');
-
-function Slack(slackConfig) {
+export default class Slack {
+  constructor(slackConfig) {
     this.config = slackConfig;
     this.webhook = new IncomingWebhook(this.config.channelUrl);
-}
+  }
 
-/**
- * Send a new message to the webhook
- * @param {String} subject
- * @param {Error} error
- * @return {Promise}
- */
-Slack.prototype.sendWebhookMessage = function (subject, error) {
-    const self = this;
-    return new Promise(function (resolve, reject) {
-        const messageBody = self.generateMessage(subject, error);
-        (async () => {
-            try {
-              const reply = await self.webhook.send(messageBody);
-              resolve(reply);
-            } catch (error) {
-              reject(error);
-            }
-        })();
-    });
-};
+  /**
+  * Send a new message to the webhook
+  * @param {String} subject
+  * @param {Error} error
+  * @return {Promise}
+  */
+  sendWebhookMessage(subject, error) {
+    return new Promise(((resolve, reject) => {
+      const messageBody = this.generateMessage(subject, error);
+      (async () => {
+        try {
+          const reply = await this.webhook.send(messageBody);
+          resolve(reply);
+        }
+        catch (err) {
+          reject(err);
+        }
+      })();
+    }));
+  }
 
-/**
- * Generate message body to send using slack SDK
- * @param {String} subject
- * @param {Error} error
- * @return {{attachments: [*]}}
- */
-Slack.prototype.generateMessage = function (subject, error) {
+  /**
+  * Generate message body to send using slack SDK
+  * @param {String} subject
+  * @param {Error} error
+  * @return {{attachments: [*]}}
+  */
+  generateMessage(subject, error) {
     const membersToAlert = this.config.notifyMembers !== undefined && this.config.notifyMembers.length > 0 ?
-        this.config.notifyMembers.map(username => `<@${username}>`).join(' ') : undefined;
+      this.config.notifyMembers.map(username => `<@${username}>`).join(' ') : undefined;
     return {
-        attachments: [{
-            fallback: error.message,
-            pretext: membersToAlert,
-            author_name: 'MongoMonitor',
-            author_link: 'https://github.com/eladnava/mongomonitor',
-            color: 'danger',
-            title: subject,
-            text: error.message
-        }]
+      attachments: [{
+        fallback: error.message,
+        pretext: membersToAlert,
+        author_name: 'MongoMonitor',
+        author_link: 'https://github.com/eladnava/mongomonitor',
+        color: 'danger',
+        title: subject,
+        text: error.message
+      }]
     };
-};
-
-module.exports = Slack;
+  }
+}
